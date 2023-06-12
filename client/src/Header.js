0bc432d16cd9aaeefbfb48945dde8b2dca7ed37c
CaseZumbrum <casezumbrum@gmail.com>
2023-06-12 07:37:50 -0400

    added links to main page

--- client/src/Header.js
+++ client/src/Header.js
@@ -6,7 +6,12 @@ export default function Header(){
         <header>
         <Link to= "/" className="logo">Case Zumbrum's Portfolio</Link>
       </header>
+      <div>
       <p>A MERN (MongoDB, Express, React, NodeJs) stack portfolio site for all of my projects! Currently running on a Raspberry Pi and using Cloud based MongoDB Atlas for storage.</p>
+      <a className="profileLink" href="https://github.com/CaseZumbrum">GitHub</a>
+      <a className="profileLink" href="mailto: casezumbrum@gmail.com">Email</a>
+      <a href="">Resume</a>
+      </div>
       </div>
     );
 }
