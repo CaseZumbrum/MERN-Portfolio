74949fd3537c5a07fcba7a5c60afda0b9d9fa15b
CaseZumbrum <casezumbrum@gmail.com>
2023-06-12 11:15:59 -0400

    updated resume

--- client/src/Header.js
+++ client/src/Header.js
@@ -10,7 +10,7 @@ export default function Header(){
       <p>A MERN (MongoDB, Express, React, NodeJs) stack portfolio site for all of my projects! Currently running on a Raspberry Pi and using Cloud based MongoDB Atlas for storage.</p>
       <a className="profileLink" href="https://github.com/CaseZumbrum">GitHub</a>
       <a className="profileLink" href="mailto: casezumbrum@gmail.com">Email</a>
-      <a href="">Resume</a>
+      <a href="https://docs.google.com/document/d/1MOTXNbck3oeZoYUyFrlqaqhfbghRPDtDoLcmGVzJUS0/edit?usp=sharing">Resume</a>
       </div>
       </div>
     );
